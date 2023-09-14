a21e70781043829f887a9593a61ada9d3c2b2d45
sattiabicsce <abisatti18@gmail.com>
2023-09-14 12:09:20 -0400

    fixed main assignment pictures

---
+++ class/sept14/script.js
@@ -0,0 +1,35 @@
+//const add = (a, b) => a+b;
+
+//const add = (a, b) => {return a + b; }
+
+//console.log(add(5,6));
+
+//const square = a => a * a;
+
+//const square = (a) => a * a;
+
+//console.log(square(5));
+
+//const hello = () => console.log("Hello me!");
+
+//hello();
+
+//const helloSpecific = userName => console.log("Hello " + userName + "!");
+
+//helloSpecific("Abi");
+
+/*
+const helloFullName = (firstName, lastName) => {
+    console.log("Hello " + firstName + " " + lastName + "!");
+    console.log("You're awesome!");
+}
+
+helloFullName("Abi", "Satti");
+
+*/
+
+/* Not ok to change a constant
+const myName = "Abi";
+myName = "portia"; */
+
+const myFunct = () => console.log("Hey you!");

3ca0e569f0f1dca0d5f7a2f85a05a7469e7d4d75
Abi <abisatti18@gmail.com>
2023-11-05 10:52:55 -0500

    fix

--- node/node.js
+++ node/node.js
@@ -9,7 +9,7 @@ app.get('/api/cars', (req, res) => {
   cars.json(cars);
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });

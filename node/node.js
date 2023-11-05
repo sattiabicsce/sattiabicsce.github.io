ac66cc878e137ff9edfe8ef3f0ad77cbb3321046
Abi <abisatti18@gmail.com>
2023-11-05 10:49:26 -0500

    fixes

--- node/node.js
+++ node/node.js
@@ -1,51 +1,12 @@
 const express = require('express');
 const app = express();
 
-const cars = [
-  {
-    brand: 'Toyota',
-    model: 'Corolla',
-    year: 2020,
-    color: 'Blue',
-    features: ['GPS', 'Bluetooth', 'Backup Camera'],
-    image: 'https://example.com/toyota_corolla.jpg'
-  },
-  {
-    brand: 'Ford',
-    model: 'Mustang',
-    year: 2019,
-    color: 'Red',
-    features: ['Leather Seats', 'Navigation System', 'Keyless Entry'],
-    image: 'https://example.com/ford_mustang.jpg'
-  },
-  {
-    brand: 'Honda',
-    model: 'Civic',
-    year: 2021,
-    color: 'Silver',
-    features: ['Apple CarPlay', 'Lane Departure Warning', 'Sunroof'],
-    image: 'https://example.com/honda_civic.jpg'
-  },
-  {
-    brand: 'Chevrolet',
-    model: 'Malibu',
-    year: 2018,
-    color: 'White',
-    features: ['Heated Seats', 'Remote Start', 'Blind Spot Monitoring'],
-    image: 'https://example.com/chevrolet_malibu.jpg'
-  },
-  {
-    brand: 'Nissan',
-    model: 'Altima',
-    year: 2017,
-    color: 'Black',
-    features: ['Push Button Start', 'Adaptive Cruise Control', 'Rear Cross Traffic Alert'],
-    image: 'https://example.com/nissan_altima.jpg'
-  }
-];
+const carsData = require('./data.json');
+
+app.use(express.static('public'));
 
 app.get('/api/cars', (req, res) => {
-  res.json(cars);
+  cars.json(cars);
 });
 
 const PORT = process.env.PORT || 3000;
